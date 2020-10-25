b42a80218d9395e3c75be2dc7e53bc66492418e1
Joe Elia <mrjoeelia@gmail.com>
2020-10-25 00:27:22 -0400

    added transistion to booking form

--- components/form/BookingForm.vue
+++ components/form/BookingForm.vue
@@ -1,40 +1,51 @@
 <template>
-  <div
-    class="z-50 modal fixed w-full h-full top-0 left-0 flex items-center justify-center"
+  <transition
+    enter-active-class="duration-200 ease-out"
+    enter-class="opacity-0 scale-95"
+    enter-to-class="opacity-100 scale-100"
+    leave-active-class="duration-200 ease-in"
+    leave-class="opacity-100 scale-100"
+    leave-to-class="opacity-0 scale-95"
   >
-    <div class="modal-overlay absolute w-full h-full bg-white opacity-95"></div>
-
-    <div class="modal-container fixed w-full h-full z-50 overflow-y-auto">
+    <div
+      class="z-50 modal fixed w-full h-full top-0 left-0 flex items-center justify-center"
+    >
       <div
-        class="modal-close absolute top-0 right-0 cursor-pointer flex flex-col items-center mt-4 mr-4 text-black text-sm z-50"
-        @click="$emit('show-booking-form', false)"
-      >
-        <svg
-          class="fill-current text-black"
-          xmlns="http://www.w3.org/2000/svg"
-          width="18"
-          height="18"
-          viewBox="0 0 18 18"
-        >
-          <path
-            d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"
-          ></path>
-        </svg>
-        (Esc)
-      </div>
+        class="modal-overlay absolute w-full h-full bg-white opacity-95"
+      ></div>
 
-      <!-- Add margin if you want to see grey behind the modal-->
-      <div class="modal-content container mx-auto h-auto text-left p-4">
-        <!--Title-->
-        <div class="flex justify-between items-center pb-2">
-          <p class="text-2xl font-exo font-bold">Book Your Appointment</p>
+      <div class="modal-container fixed w-full h-full z-50 overflow-y-auto">
+        <div
+          class="modal-close absolute top-0 right-0 cursor-pointer flex flex-col items-center mt-4 mr-4 text-black text-sm z-50"
+          @click="$emit('show-booking-form', false)"
+        >
+          <svg
+            class="fill-current text-black"
+            xmlns="http://www.w3.org/2000/svg"
+            width="18"
+            height="18"
+            viewBox="0 0 18 18"
+          >
+            <path
+              d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"
+            ></path>
+          </svg>
+          (Esc)
         </div>
 
-        <!--Body-->
-        <flow-form v-bind:questions="questions" />
+        <!-- Add margin if you want to see grey behind the modal-->
+        <div class="modal-content container mx-auto h-auto text-left p-4">
+          <!--Title-->
+          <div class="flex justify-between items-center pb-2">
+            <p class="text-2xl font-exo font-bold">Book Your Appointment</p>
+          </div>
+
+          <!--Body-->
+          <flow-form v-bind:questions="questions" />
+        </div>
       </div>
     </div>
-  </div>
+  </transition>
 </template>
 
 <script>
