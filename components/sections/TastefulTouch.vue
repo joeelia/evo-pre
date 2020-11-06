8b2dc9bc2337b4f1db419a48a0b0f6779150f2bb
Joe Elia <mrjoeelia@gmail.com>
2020-11-06 00:25:07 -0500

    added footer

--- components/sections/TastefulTouch.vue
+++ components/sections/TastefulTouch.vue
@@ -1,6 +1,17 @@
 <template>
-  <div class="flex max-h-full">
-    <section class="grid grid-cols-3 text-gray-900 bg-pink-500 sm:grid-cols-2">
+  <div
+    class="relative flex w-full max-h-full py-20 pb-20 overflow-hidden bg-pink-500 md:pt-40 md:pb-64"
+  >
+    <svg
+      class="absolute top-0 w-full text-pink-200 fill-current"
+      viewBox="0 0 1400 50"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M0 50C309.151 16.667 542.484 0 700 0c157.516 0 390.849 16.667 700 50V0H0v50z"
+      ></path>
+    </svg>
+    <section class="grid grid-cols-3 text-gray-900 sm:grid-cols-2">
       <div class="z-10 h-full col-span-2 sm:col-span-1 place-self-end">
         <div class="flex items-center w-full h-full">
           <div
@@ -33,6 +44,15 @@
         />
       </div>
     </section>
+    <svg
+      class="absolute bottom-0 w-full text-pink-200 fill-current"
+      viewBox="0 0 1400 74"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M0 24C87.243 11.422 173.12 5.133 257.633 5.133 468.305 5.133 578.027 74 700 74c136.015 0 290.882-96.208 481.234-68.867C1268.807 17.71 1341.73 24 1400 24v50H0V24z"
+      ></path>
+    </svg>
   </div>
 </template>
 <script>
