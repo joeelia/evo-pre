bd85a05c9d3c7f7aa62bddf565815b94d79e5bd3
Joe Elia <mrjoeelia@gmail.com>
2020-11-07 00:32:55 -0500

    made service squares bigger

--- components/sections/Services.vue
+++ components/sections/Services.vue
@@ -6,37 +6,37 @@
           class="box-content flex flex-row flex-no-wrap max-w-full py-2 space-x-6 overflow-x-scroll md:space-x-2 md:space-y-6 md:overflow-auto md:flex-wrap hide-scrollbar"
         >
           <li
-            class="flex flex-col items-center w-auto h-12 p-10 ml-2 text-xs font-medium leading-4 text-center text-white transition duration-150 ease-in-out bg-indigo-600 border border-transparent rounded cursor-pointer md:min-w-full xl:w-auto hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 md:flex-row md:p-8"
+            class="flex flex-col items-center w-auto h-12 text-xs font-medium leading-4 text-center text-white transition duration-150 ease-in-out bg-indigo-600 border border-transparent rounded cursor-pointer p-15 md:min-w-full xl:w-auto hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 md:flex-row md:p-8"
             @click="serviceClicked(service.weightLoss)"
           >
             Weight Loss
           </li>
           <li
-            class="flex flex-col items-center w-auto h-12 p-10 ml-2 text-xs font-medium leading-4 text-center text-white transition duration-150 ease-in-out bg-indigo-600 border border-transparent rounded cursor-pointer md:min-w-full xl:w-auto hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 md:flex-row md:p-8"
+            class="flex flex-col items-center w-auto h-12 text-xs font-medium leading-4 text-center text-white transition duration-150 ease-in-out bg-indigo-600 border border-transparent rounded cursor-pointer p-15 md:min-w-full xl:w-auto hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 md:flex-row md:p-8"
             @click="serviceClicked(service.prp)"
           >
             PRP
           </li>
           <li
-            class="flex flex-col items-center w-auto h-12 p-10 ml-2 text-xs font-medium leading-4 text-center text-white transition duration-150 ease-in-out bg-indigo-600 border border-transparent rounded cursor-pointer md:min-w-full xl:w-auto hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 md:flex-row md:p-8"
+            class="flex flex-col items-center w-auto h-12 text-xs font-medium leading-4 text-center text-white transition duration-150 ease-in-out bg-indigo-600 border border-transparent rounded cursor-pointer p-15 md:min-w-full xl:w-auto hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 md:flex-row md:p-8"
             @click="serviceClicked(service.botox)"
           >
             Botox
           </li>
           <li
-            class="flex flex-col items-center w-auto h-12 p-10 ml-2 text-xs font-medium leading-4 text-center text-white transition duration-150 ease-in-out bg-indigo-600 border border-transparent rounded cursor-pointer md:min-w-full xl:w-auto hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 md:flex-row md:p-8"
+            class="flex flex-col items-center w-auto h-12 text-xs font-medium leading-4 text-center text-white transition duration-150 ease-in-out bg-indigo-600 border border-transparent rounded cursor-pointer p-15 md:min-w-full xl:w-auto hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 md:flex-row md:p-8"
             @click="serviceClicked(service.fillers)"
           >
             Fillers
           </li>
           <li
-            class="flex flex-col items-center w-auto h-12 p-10 ml-2 text-xs font-medium leading-4 text-center text-white transition duration-150 ease-in-out bg-indigo-600 border border-transparent rounded cursor-pointer md:min-w-full xl:w-auto hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 md:flex-row md:p-8"
+            class="flex flex-col items-center w-auto h-12 text-xs font-medium leading-4 text-center text-white transition duration-150 ease-in-out bg-indigo-600 border border-transparent rounded cursor-pointer p-15 md:min-w-full xl:w-auto hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 md:flex-row md:p-8"
             @click="serviceClicked(service.scalpThearpy)"
           >
             Scalp Thearpy
           </li>
           <li
-            class="flex flex-col items-center w-auto h-12 p-10 ml-2 text-xs font-medium leading-4 text-center text-white transition duration-150 ease-in-out bg-indigo-600 border border-transparent rounded cursor-pointer md:min-w-full xl:w-auto hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 md:flex-row md:p-8"
+            class="flex flex-col items-center w-auto h-12 text-xs font-medium leading-4 text-center text-white transition duration-150 ease-in-out bg-indigo-600 border border-transparent rounded cursor-pointer p-15 md:min-w-full xl:w-auto hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 md:flex-row md:p-8"
             @click="serviceClicked(service.nutrition)"
           >
             Nutrition
