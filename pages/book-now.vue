e0e6b366cab877f6878ff346458e6ffc416b2123
Joe Elia <mrjoeelia@gmail.com>
2021-01-02 21:05:17 -0800

    updated iframe on booking form

--- pages/book-now.vue
+++ pages/book-now.vue
@@ -4,13 +4,13 @@
       src="https://app.acuityscheduling.com/schedule.php?owner=21606081"
       title="Schedule Appointment"
       width="100%"
-      height="800"
       frameBorder="0"
     ></iframe>
     <script
       src="https://embed.acuityscheduling.com/js/embed.js"
       type="text/javascript"
     ></script>
+    <LightFooter />
   </div>
 </template>
 
