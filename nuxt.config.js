15b9f38cca2e29beb4c4889116cbfcd0b1a61a92
Joe Elia <mrjoeelia@gmail.com>
2021-01-24 21:46:31 -0800

    fixed tailwind 2.0 issue

--- nuxt.config.js
+++ nuxt.config.js
@@ -52,7 +52,6 @@ export default {
     '@nuxtjs/eslint-module',
     // Doc: https://github.com/nuxt-community/nuxt-tailwindcss
     '@nuxtjs/tailwindcss',
-    '@teamnovu/nuxt-breaky',
   ],
   tailwindcss: {
     exposeConfig: true,
