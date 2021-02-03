699386997c9d83a472ec17d8a7afb9c1db4cc3f0
Joe Elia <mrjoeelia@gmail.com>
2021-02-03 12:56:20 -0800

    updated padding on nav for mobileadded shadow

--- components/Nav2.vue
+++ components/Nav2.vue
@@ -3,13 +3,13 @@
   <header>
     <div class="z-40 sticky top-0 antialiased bg-gray-100 dark-mode:bg-gray-900">
       <div class="w-full text-gray-700 bg-white dark-mode:text-gray-200 dark-mode:bg-gray-800">
-        <div v-click-outside="hideNavs" class="flex flex-col max-w-screen-xl px-4 mx-auto md:items-center md:justify-between md:flex-row md:px-6 lg:px-8">
+        <div v-click-outside="hideNavs" class="flex flex-col max-w-screen-xl px-0 mx-auto md:items-center md:justify-between md:flex-row md:px-6 lg:px-8">
           <div class="flex flex-row items-center justify-between p-4">
             <Logo id="logo" />
             <nuxt-link
-              v-if="$route.fullPath.indexOf('book-now')"
+              v-if="$route.fullPath.indexOf('/book-now')"
               to="/book-now"
-              class="ml-8 md:hidden whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-gradient-to-r from-orange-500 to-pink-500 hover:to-pink-600"
+              class="md:hidden whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-gradient-to-r from-orange-500 to-pink-500 hover:to-pink-600"
             >
               Book Now
             </nuxt-link>
@@ -20,16 +20,16 @@
               </svg>
             </button>
           </div>
-          <nav :class="{flex: openMobileNav, hidden: !openMobileNav}" class="flex-col flex-grow pb-4 md:pb-0 md:flex md:justify-end md:flex-row">
-            <nuxt-link to="/pricing" class="px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 md:ml-4 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline cursor-pointer">Pricing</nuxt-link>
-            <span class="px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 md:ml-4 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline cursor-pointer" @click="contactModalEmit(true)">Contact</span>
+          <nav :class="{flex: openMobileNav, hidden: !openMobileNav}" class="px-2 flex-col flex-grow pb-4 md:pb-0 md:flex md:justify-end md:flex-row">
+            <nuxt-link to="/pricing" class="border-2 border-pink-400 md:border-0 md:border-transparent px-4 py-4 md:py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 md:ml-4 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline cursor-pointer">Pricing</nuxt-link>
+            <span class="border-2 border-pink-400 md:border-0 md:border-transparent px-4 py-4 md:py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 md:ml-4 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline cursor-pointer" @click="contactModalEmit(true)">Contact</span>
             <div class="relative">
-              <button :class="{'bg-gray-200': !$route.fullPath.indexOf('/services')}" @click="openSubNav = !openSubNav" class="flex flex-row text-gray-900 items-center w-full px-4 py-2 mt-2 text-sm font-semibold text-left bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:focus:bg-gray-600 dark-mode:hover:bg-gray-600 md:w-auto md:inline md:mt-0 md:ml-4 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline">
+              <button :class="{'bg-gray-200': !$route.fullPath.indexOf('/services')}" @click="openSubNav = !openSubNav" class="border-2 border-pink-400 md:border-0 md:border-transparent flex flex-row text-gray-900 items-center w-full px-4 py-4 md:py-2 mt-2 text-sm font-semibold text-left bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:focus:bg-gray-600 dark-mode:hover:bg-gray-600 md:w-auto md:inline md:mt-0 md:ml-4 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline">
                 <span>Services</span>
                 <svg fill="currentColor" viewBox="0 0 20 20" :class="{'rotate-180': openSubNav, 'rotate-0': !openSubNav}" class="inline w-4 h-4 mt-1 ml-1 transition-transform duration-200 transform md:-mt-1"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
               </button>
-              <div v-show="openSubNav" x-transition:enter="transition ease-out duration-100" x-transition:enter-start="transform opacity-0 scale-95" x-transition:enter-end="transform opacity-100 scale-100" x-transition:leave="transition ease-in duration-75" x-transition:leave-start="transform opacity-100 scale-100" x-transition:leave-end="transform opacity-0 scale-95" class="absolute right-0 w-full md:max-w-screen-sm md:w-screen mt-2 origin-top-right">
-                <div class="px-2 pt-2 pb-4 bg-white rounded-md shadow-lg dark-mode:bg-gray-700">
+              <div v-show="openSubNav" x-transition:enter="transition ease-out duration-100" x-transition:enter-start="transform opacity-0 scale-95" x-transition:enter-end="transform opacity-100 scale-100" x-transition:leave="transition ease-in duration-75" x-transition:leave-start="transform opacity-100 scale-100" x-transition:leave-end="transform opacity-0 scale-95" class="shadow-2xl absolute right-0 w-full md:max-w-screen-sm md:w-screen mt-2 origin-top-right">
+                <div class="px-2 pt-2 pb-4 bg-white rounded-md shadow-2xl dark-mode:bg-gray-700">
                   <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
                      <nuxt-link
                       v-for="(service, i) in services"
@@ -50,7 +50,7 @@
               </div>
             </div>
               <nuxt-link
-              v-if="$route.fullPath.indexOf('book-now')"
+              v-if="$route.fullPath.indexOf('/book-now')"
               to="/book-now"
               class="ml-10 hidden md:inline-flex whitespace-nowrap items-center justify-center px-4 py-1 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-gradient-to-r from-orange-500 to-pink-500 hover:to-pink-600"
             >
@@ -124,22 +124,20 @@
                 Contact Us!
               </h3>
               <div class="mt-2 mb-40">
-                <a
-                  type="button"
+                <span
                   class="inline-flex justify-center w-full px-4 py-2 text-base font-medium leading-6 text-white transition duration-150 ease-in-out bg-green-300 bg-pink-600 border border-transparent shadow-sm bg-red-saroki hover:bg-red-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo sm:text-sm sm:leading-5"
                   @click="gaEvent('contact_modal_sms')"
                   href="sms:+12485952363&body=Hey EVO Body Image!"
                 >
                   Text Us
-                </a>
-                <a
-                  type="button"
+                </span>
+                <span
                   class="inline-flex justify-center w-full px-4 py-2 mt-10 text-base font-medium leading-6 text-white transition duration-150 ease-in-out bg-green-300 bg-pink-600 border border-transparent shadow-sm bg-red-saroki hover:bg-red-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo sm:text-sm sm:leading-5"
                   @click="gaEvent('contact_modal_call_spa')"
                   href="tel:+(248) 878-6639"
                 >
                   Call the Spa
-                </a>
+                </span>
               </div>
             </div>
           </div>
